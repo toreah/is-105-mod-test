24c9e5a3d1316e130fcca25303b236224d63869d
toreah <toreah@uia.no>
2022-01-28 01:03:49 +0100

    Myquotes

---
+++ modul1/Myquotes/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"main.go/myquotes"
+)
+
+func main() {
+	myquotes.M()
+}
---
+++ modul1/Myquotes/myquotes/myquotes.go
@@ -0,0 +1,14 @@
+package myquotes
+
+import (
+	"fmt"
+
+	"rsc.io/quote"
+)
+
+func M() {
+	fmt.Println(quote.Go())
+	fmt.Println(quote.Hello())
+	fmt.Println(quote.Glass())
+	fmt.Println(quote.Opt())
+}
